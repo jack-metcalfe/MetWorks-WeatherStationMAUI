c12d75baca759a84a940150fe04a2e4a3ba1c52a
VikingsFan1024 <Jack.Metcalfe@live.com>
2026-01-14 22:08:50 -0600

    Removed dead assembly project

--- src/settings_override_structures/OverridesMetadata.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SettingsOverrideStructures;
-public record OverridesMetadata : IOverridesMetadata
-{
-    public bool BufferingAllowed { get; init; } = true;
-    public bool LockingSupported { get; init; } = false;
-}
--- src/settings_override_structures/OverridesModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SettingsOverrideStructures;
-public record OverridesModel : IOverridesModel
-{
-    public Dictionary<string, string> Settings { get; init; } = new();
-    public Dictionary<string, string> Secrets { get; init; } = new();
-    public Dictionary<string, string> Routing { get; init; } = new();
-    public OverridesMetadata Metadata { get; init; } = new();
-}
