8ac139cc32d522aa45db13d135d14d2f1427eef5
VikingsFan1024 <Jack.Metcalfe@live.com>
2026-01-14 22:11:11 -0600

    Removed another now dead assembly

--- src/maui_setting_override_providers/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-﻿global using Utility;
-global using YamlDotNet.Serialization;
-global using YamlDotNet.Serialization.NamingConventions;
-global using Interfaces;
-global using StaticDataStore;
-global using Settings;
